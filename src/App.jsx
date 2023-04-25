878b277b8fe4eb7f96e82b79751ec332b6a51aae
Gian Junius <gianjunius54@gmail.com>
2023-04-25 17:12:44 +0700

    card height

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App block text-white font-poppins bg-oxford bg-cover bg-no-repeat h-screen">
       <Header/>
       <div className="flex justify-center items-center ">
-      <div className="card h-[500px] w-[420px] rounded-[50px] backdrop-filter backdrop-blur-xl flex justify-center my-10 ">
+      <div className="card h-[550px] w-[420px] rounded-[50px] backdrop-filter backdrop-blur-xl flex justify-center my-10 ">
       <Timer/>
       <Song className="items-center"/>
       </div>
