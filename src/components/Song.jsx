087f9e080080cc0efa9d3467469c2ad43f12c90f
Gian Junius <gianjunius54@gmail.com>
2023-04-25 17:10:53 +0700

    margin-top

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,8 +1,8 @@
 const Song = () => {
     return (
-        <div className="grid absolute inset-x-0 bottom-20">
+        <div className="grid absolute inset-x-0 bottom-20 mt-3">
         <div className="flex justify-center items-center">
-        <h1 className="text-lg badge glass mt-3">Relaxing song</h1>
+        <h1 className="text-lg badge glass">Relaxing song</h1>
         </div>
         <div className="flex justify-center items-center m-4">
         <iframe id="spotify" width="80%" height="80" frameborder="0" allowfullscreen="" data-src="https://open.spotify.com/embed/playlist/4Zjli1P13J5mmSCD5iKAXK?theme=0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" src="https://open.spotify.com/embed/album/05WYfOdYRlapM87Fy9r2rs?utm_source=generator"></iframe>
