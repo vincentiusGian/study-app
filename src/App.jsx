8da600c857902e3a62cae753e5725d71f21d54d7
Gian Junius <gianjunius54@gmail.com>
2023-04-25 17:27:44 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Song from './components/Song';
 function App() {
   return (
   
-    <div className="App block text-white font-poppins bg-oxford bg-cover bg-no-repeat h-screen">
+    <div className="App block text-white dark:text-white font-poppins bg-oxford bg-cover bg-no-repeat h-screen">
       <Header/>
       <div className="flex justify-center items-center ">
       <div className="card h-[550px] w-[420px] rounded-[50px] backdrop-filter backdrop-blur-xl flex justify-center my-10 ">
