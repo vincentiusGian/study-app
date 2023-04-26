4b0fb8f8b8eeaa8f10db12926326e2d6bc1f2f6b
Gian Junius <gianjunius54@gmail.com>
2023-04-26 09:54:43 +0700

    fakkfd

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
 
 export default function Timer() {
-    const [minutes, setMinutes] = useState(24);
-    const [seconds, setSeconds] = useState(59);
+    const [minutes, setMinutes] = useState(25);
+    const [seconds, setSeconds] = useState(0);
     const [displayMsg, setDisplayMsg] = useState(false);
-    const [isActive, setIsActive] = useState(false);
+    
 
 
     useEffect(() => {
         
         let interval = setInterval(() => {
             clearInterval(interval);
-            if (!isActive && seconds === 0) {
-                if(!isActive && minutes !== 0) {
+            if ( seconds === 0) {
+                if( minutes !== 0) {
                     setSeconds(59);
                     setMinutes(minutes - 1);
                 } else {
@@ -23,26 +23,15 @@ export default function Timer() {
                     setMinutes(minutes);
                     setDisplayMsg(!displayMsg);
                 } 
-            } else if(isActive && seconds !== 0){
+            } else if(seconds !== 0){
                 setSeconds(seconds - 1);
             }
             
         }, 1000);
-    }, [seconds, isActive]);
+    }, [seconds]);
 
-    const toggleStart = () => {
-        setIsActive(!isActive);
-    };
+    
 
-    const breakTime = () => {
-        let minutes = displayMsg ? 24 : 4;
-        let seconds = 59;
-        
-        setSeconds(seconds);
-        setMinutes(minutes);
-        setDisplayMsg(!displayMsg);
-        setIsActive(isActive);
-    }
 
     const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
     const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
@@ -57,11 +46,7 @@ export default function Timer() {
                 {displayMsg && <h1>Break time!</h1>}
             </div>
             <div className="text-9xl ">
-                {toggleStart ? <h1>{timerMinutes}:{timerSeconds}</h1> : <h1>25:00</h1> }
-            </div>
-            <div className="flex justify-center space-x-4">
-            <a className="btn glass rounded-full dark:text-white" onClick={toggleStart}>{!isActive ? 'start' : 'pause'}</a>
-            <a className="btn glass rounded-full dark:text-white" onClick={breakTime}>{!displayMsg ? 'start break' : 'end break'}</a>
+                 <h1>{timerMinutes}:{timerSeconds}</h1>
             </div>
             <div className="chat chat-end absolute inset-x-0 bottom-7">
   <div className="chat-bubble chat-bubble-secondary">Pause first before you start or end breaks.</div>
