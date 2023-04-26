ca3d0411391923a3ac6e846e657add62b3c09010
Gian Junius <gianjunius54@gmail.com>
2023-04-26 09:32:45 +0700

    song

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,8 +1,19 @@
 const Song = () => {
+    const playlistId = '0FFz6sc0xHLTwohftrnKsa';
+    
     return (
         <div className="grid absolute inset-x-0 bottom-20 mt-3 ">
         <div className="flex justify-center items-center m-4 ">
-        <iframe src="https://open.spotify.com/embed/playlist/4Zjli1P13J5mmSCD5iKAXK?theme=0" width="80%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src={`https://open.spotify.com/embed/playlist/0FFz6sc0xHLTwohftrnKsa?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
         </div>
         </div>
     )
